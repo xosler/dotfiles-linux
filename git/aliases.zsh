12906d95bb7884118374330d8496ac1e5c14d497
Carlos Alexandro Becker <caarlos0@gmail.com>
2012-11-24 21:22:14 -0200

    one more alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,6 +6,7 @@ alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gcm='git commit -m'
+alias gcam='git commit -am'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
