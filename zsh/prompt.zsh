10b42c5827f054fafce00e0caa2106ec2ba4ec6b
Carlos Alexandro Becker <caarlos0@gmail.com>
2012-11-26 10:44:17 -0200

    trailing pyc sobrando, removido graph do svn (muito lento)

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -78,7 +78,7 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)$(svn_graph)\n› '
+export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
