043290e8245abe653b8e431865b188c2ba47ffa8
Carlos Alexandro Becker <caarlos0@gmail.com>
2012-12-19 16:37:07 -0200

    added a ~filtered~ svn stat (REVERSE GREP WITH RAGE BRO)

--- svn/aliases.zsh
+++ svn/aliases.zsh
@@ -3,3 +3,8 @@ alias svns='svn stat'
 alias svnl='svn log -l 10'
 alias svnc='svn commit'
 alias svndv='svn diff | vim -'
+
+svs() {
+  svn stat | grep -v "classpath" | grep -v "/bin" | grep -v ".settings" | grep -v ".project" | grep -v "target"
+}
+
